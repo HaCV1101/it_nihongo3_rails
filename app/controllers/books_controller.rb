5670048272ad68fdb935b0395702d99331a70dfc
Thu Ha <dinhthithuha1101@gmail.com>
2023-01-31 08:54:29 +0000

    Add detail book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,7 +65,7 @@ class BooksController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
-    def book_params
-      params.require(:book).permit(:title, :number_of_pages, :image)
-    end
+   def book_params
+      params.require(:book).permit(:title, :number_of_pages, :detail ,:image)
+   end
 end
